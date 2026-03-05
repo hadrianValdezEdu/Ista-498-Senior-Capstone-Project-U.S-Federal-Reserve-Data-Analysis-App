503e69feed9b123d4853ee1cdd299ce3b9443271
hadrianValdezEdu <hadrianvaldez@arizona.edu>
2026-03-04 18:32:10 -0700

    Search() class completed. Next step, Excel integration

--- app/data_retrival.py
+++ app/data_retrival.py
@@ -31,13 +31,41 @@ class Search():
         )
         return requests.get(url).json()
     
-    def get_category_id(self):
+    def get_series(self, category_id):
+        url = (
+            "https://api.stlouisfed.org/fred/category/series"
+            f"?category_id={category_id}&api_key={self.api_key}&file_type=json"
+        )
+        return requests.get(url).json()
+    
+    def get_series_df(self, category_id):
+        # assigns this data var as a dict
+        data = self.get_series(category_id)
+
+        # returns a list containing ONLY what was in the key "seriess"
+        series_lst = data.get("seriess", [])
+
+        rows = []
+
+        for series in series_lst:
+            rows.append({
+                "series_id": series["id"],
+                "title": series["title"],
+                "frequency": series["frequency"],
+                "units": series["units"],
+                "seasonal_adjustment": series["seasonal_adjustment"]
+                })
+        
+        return pd.DataFrame(rows)
+    
+    def get_category_id(self, category_id=0):
         # pandas has built in dataframe compatibility for lists containing dicts.
         # This pd.Dataframelist([dict{}]) data structure will be needed for
         # Excel and JSON compatibility.
         selection_df = []
 
-        for lst in self.get_subcategories().values():
+        # call helper function, it returns a dictionary
+        for lst in self.get_subcategories(category_id).values():
             for dictionary in lst:
                 selection_df.append({
                     "category": dictionary["name"],
@@ -60,8 +88,19 @@ class Search():
         df["value"] = pd.to_numeric(df["value"], errors="coerce")
 
         return df[["date", "value"]]
+    
+    # this function is safe to delete whenever needed.
+def example():
+    print("test_var.get_category_id()\n", test_var.get_category_id())
+    print("test_var.get_data\n", test_var.get_data("GDP"))
+    print("test_var.get_subcategories(category_id=32992)\n", test_var.get_subcategories(category_id=32992))
+    print("test_var.get_series(32992)\n",test_var.get_series(32992))
 
 if __name__ == "__main__":
-    df = get_data(series_id, api_key)
-    df.to_csv('gdp_data.csv', index=True)
-    print(df)
+    #df = get_data(series_id, api_key)
+    #df.to_csv('gdp_data.csv', index=True)
+    
+    # example of Search()
+    # api_key = "ENTER YOUR API KEY HERE"
+    test_var = Search(api_key = "")
+    example()
